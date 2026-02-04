0ce55702d18b32b903009e16fd6426ef5b4f14ae
ludmila tawane rodrigues dos anjos <ludmila.tawane@escolar.ifrn.edu.br>
2026-02-04 09:46:42 -0300

    mudei

--- routes/animais.js
+++ routes/animais.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
             donos.nome AS dono_nome,
             donos.cpf
         FROM animais
-        LEFT JOIN donos ON animais.cpf_dono = donos.cpf
+        LEFT JOIN donos ON animais.id_dono = donos.id_dono
         ORDER BY animais.nome
     `;
 
@@ -34,14 +34,14 @@ router.get('/', (req, res) => {
 // SALVAR ANIMAL
 // =========================
 router.post('/add', (req, res) => {
-    const { nome, idade, especie, raca, cpf_dono } = req.body;
+    const { nome, idade, especie, raca, id_dono } = req.body;
 
     const sql = `
-        INSERT INTO animais (nome, idade, especie, raca, cpf_dono)
+        INSERT INTO animais (nome, idade, especie, raca, id_dono)
         VALUES (?, ?, ?, ?, ?)
     `;
 
-    db.query(sql, [nome, idade, especie, raca, cpf_dono], err => {
+    db.query(sql, [nome, idade, especie, raca, id_dono], err => {
         if (err) {
             console.log(err);
             return res.send('Erro ao salvar animal');
