91c65c18bddcc104775c7a8dacfbaea42ed364ee
ludmila tawane rodrigues dos anjos <ludmila.tawane@escolar.ifrn.edu.br>
2026-02-05 19:34:09 -0300

    mudarrrr

--- routes/servicos.js
+++ routes/servicos.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
             return res.send('Erro ao carregar serviços');
         }
 
-        res.render('serviços-list.ejs', { servicos });
+        res.render('servicos-list.ejs', { servicos });
     });
 });
 
