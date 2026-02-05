6da1b5ad24c7136a79b1feebd774b7cdc38f67e8
ludmila tawane rodrigues dos anjos <ludmila.tawane@escolar.ifrn.edu.br>
2026-02-05 20:50:26 -0300

    mdai

--- routes/veterinarios.js
+++ routes/veterinarios.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const regexCRMV = /^CRMV-[A-Z]{2} [0-9]{5}$/;
+
+router.post('/add', (req, res) => {
+    const { nome, especialidade, celular, crmv } = req.body;
+
+    if (!regexCRMV.test(crmv)) {
+        return res.send('CRMV inválido. Use o formato: CRMV-SP 12345');
+    }
+
+    const sql = `
+        INSERT INTO veterinarios (nome, especialidade, celular, crmv)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    db.query(sql, [nome, especialidade, celular, crmv], err => {
+        if (err) {
+            console.error(err);
+            return res.send('Erro ao salvar veterinário');
+        }
+
+        res.redirect('/veterinarios');
+    });
+});
 
 // LISTAR
 router.get('/', (req, res) => {
